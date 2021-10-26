fcbc7592badccbd98b9e0e9b557d5642a59037eb
hiltona7 <hiltona7@mailbox.winthrop.edu>
2021-10-26 17:58:01 -0400

    startup

--- telecom-angular/src/app/app-routing.module.ts
+++ telecom-angular/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PlansComponent } from './plans/plans.component';
+import { AccountComponent } from './account/account.component';
+import { HomeComponent } from './home/home.component';
+import { PurchaseComponent } from './purchase/purchase.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'plans', component: PlansComponent
+  },
+  {
+    path: 'account', component: AccountComponent
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'purchase', component: PurchaseComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- telecom-angular/src/app/app.module.ts
+++ telecom-angular/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
+import { PlansComponent } from './plans/plans.component';
+import { PurchaseComponent } from './purchase/purchase.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AccountComponent,
+    PlansComponent,
+    PurchaseComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
